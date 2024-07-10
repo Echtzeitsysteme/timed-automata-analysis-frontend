2be8a2b735b77ff8873dfc20153c70a36846dc90
Josef552 <105931716+Josef552@users.noreply.github.com>
2024-07-10 23:03:59 +0200

    "fixed" parser, but no output yet...

--- src/view/UploadButton.tsx
+++ src/view/UploadButton.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 
+import timedAutomata from '../parser/timedAutomata';
+console.log(timedAutomata);
+
 export interface OpenedDocs {
   //unbenennen in OpenedDocs oder so
   //fileContents: string;
@@ -12,26 +15,48 @@ const UploadButton: React.FC<OpenedDocs> = () => {
     };
     //TODO der überprüft ja momentan noch nicht wirklich, ob das eine .tck Datei ist...
 
-    if (!inputElem.files[0]) {
+    if (!inputElem.files[0] || !inputElem.files[0].name.endsWith('.tck')) {
+      console.log('Invalid or no File');
       return;
     }
     console.log(inputElem.files[0]); //eingelesene File
 
-    //das alles vllcht in einen "Promise"-Block reinpacken???
-
-    //definition für fileReader
     const fileReader = new FileReader();
     fileReader.onload = () => {
       const fileContent = fileReader.result as string;
       console.log(fileContent);
 
-      //TODO und hier dann in das onload selbst Parser noch rein
+      //TODO das einkommentieren, sobald import geht
+      const parser = new timedAutomata.Parser();
+
+      const parsedData = parser.parse(fileContent);
+      console.log('parsedData', parsedData); //gibt einfach "true" zurück...
     };
     fileReader.readAsText(inputElem.files[0]);
-    //TODO Parser Class in anderer Datei erstellen für String auslesen oder in onload selbst
 
     //TODO Muss ich die File noch irgendwo anders abspeichern?
   };
+
+  /**
+  const myPromise: Promise<any> = new Promise((resolve, reject)) => {
+    parser.parse(inputFile);
+  }
+
+  //oder "File", wenn ich den fileReader hier mit reinpacke
+  async function parseData(inputFile: string){
+    try{
+      const parsedData = await parser.parse(inputFile);
+      console.log(parsedData);
+      //ODER:
+      // return parser.parse(inputFile);
+      console.log('a');
+    }
+    catch(error){
+      console.log(error);
+    }
+
+  }
+   **/
 
   //TODO hier noch das "Upload file" in diese Localization file tun
   return (
