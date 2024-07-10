0b808098c7fbad7d6193326fd4e5355734ad482f
Josef552 <105931716+Josef552@users.noreply.github.com>
2024-07-10 23:03:59 +0200

    "fixed" parser, but no output yet...

--- src/parser/timedAutomata.jison
+++ src/parser/timedAutomata.jison
@@ -8,19 +8,47 @@
 "clock"                     %{ return 'TOK_CLOCK' %}
 "edge"                      %{ return 'TOK_EDGE' %}
 "event"                     %{ return 'TOK_EVENT' %}
-"identifier"                %{ return 'TOK_ID' %}
 "int"                       %{ return 'TOK_INT' %}
 "location"                  %{ return 'TOK_LOCATION' %}
 "process"                   %{ return 'TOK_PROCESS' %}
 "sync"                      %{ return 'TOK_SYNC' %}
 "system"                    %{ return 'TOK_SYSTEM' %}
-[a-zA-Z]([a-zA-Z0-9_])*     %{ return 'TOK_TEXT' %}
+"initial"                   %{ return 'TOK_INIT' %}
+"labels"                    %{ return 'TOK_LABELS' %}
+"invariant"                 %{ return 'TOK_INVAR' %}
+"committed"                  %{ return 'TOK_COMMIT' %}
+"urgent"                    %{ return 'TOK_URGENT' %}
+"provided"                  %{ return 'TOK_PROV' %}
+"do"                        %{ return 'TOK_DO' %}
+"if"                        %{ return 'TOK_IF' %}
+"while"                     %{ return 'TOK_WHILE' %}
+"nop"                       %{ return 'TOK_NOP' %}
+"local"                     %{ return 'TOK_LOCAL' %}
 [0-9]+                      %{ return 'TOK_INTEGER' %}'
+[a-zA-Z]([a-zA-Z0-9_])*     %{ return 'TOK_ID' %}
 ":"                         %{ return 'TOK_COLON' %}       
 "@"                         %{ return 'TOK_AT' %}
+"!"                         %{ return 'TOK_EXMARK' %}
 "?"                         %{ return 'TOK_QMARK' %}
 "{"                         %{ return 'TOK_LBRACE' %}
 "}"                         %{ return 'TOK_RBRACE' %}
+","                         %{ return 'TOK_COMMA' %}
+"&&"                        %{ return 'TOK_AND' %}
+"+"                         %{ return 'TOK_PLUS' %}
+"-"                         %{ return 'TOK_MINUS' %}
+"*"                         %{ return 'TOK_MUL' %}
+"/"                         %{ return 'TOK_DIV' %}
+"%"                         %{ return 'TOK_PERCENT' %}
+"=="                        %{ return 'TOK_EQ' %}
+"!="                        %{ return 'TOK_NEQ' %}
+"<="                        %{ return 'TOK_LEQ' %}
+">="                        %{ return 'TOK_GEQ' %}
+"<"                         %{ return 'TOK_LT' %}
+">"                         %{ return 'TOK_GT' %}
+"="                         %{ return 'TOK_SET' %}
+"("                         %{ return 'TOK_LBRACKET' %}
+")"                         %{ return 'TOK_RBRACKET' %}
+";"                         %{ return 'TOK_SEMICOLON' %}
 <<EOF>>                     %{ return 'TOK_EOF' %} 
 /lex
 
@@ -54,8 +82,8 @@ item
 
 
 systemDef
- : TOK_SYSTEM TOK_COLON TOK_TEXT attributeList
- | TOK_SYSTEM TOK_COLON TOK_TEXT
+ : TOK_SYSTEM TOK_COLON TOK_ID attributeList
+ | TOK_SYSTEM TOK_COLON TOK_ID
  ;
 
 processDef
@@ -75,37 +103,23 @@ clockDef
 
 intDef
  : TOK_INT TOK_COLON TOK_INTEGER TOK_COLON TOK_INTEGER TOK_COLON TOK_INTEGER TOK_COLON TOK_INTEGER TOK_COLON TOK_ID attributeList
+ | TOK_INT TOK_COLON TOK_INTEGER TOK_COLON TOK_INTEGER TOK_COLON TOK_INTEGER TOK_COLON TOK_INTEGER TOK_COLON TOK_ID
  ;
 
 locationDef
- : TOK_LOCATION TOK_COLON TOK_PROCESS TOK_COLON TOK_ID attributeList
- | TOK_LOCATION TOK_COLON TOK_PROCESS TOK_COLON TOK_ID
+ : TOK_LOCATION TOK_COLON TOK_ID TOK_COLON TOK_ID attributeList
+ | TOK_LOCATION TOK_COLON TOK_ID TOK_COLON TOK_ID
  ;
 
+
 edgeDef
- : TOK_EDGE TOK_COLON TOK_PROCESS TOK_COLON TOK_LOCATION TOK_COLON TOK_LOCATION TOK_COLON TOK_EVENT attributeList
- | TOK_EDGE TOK_COLON TOK_PROCESS TOK_COLON TOK_LOCATION TOK_COLON TOK_LOCATION TOK_COLON TOK_EVENT
+ : TOK_EDGE TOK_COLON TOK_ID TOK_COLON TOK_ID TOK_COLON TOK_ID TOK_COLON TOK_ID attributeList
+ | TOK_EDGE TOK_COLON TOK_ID TOK_COLON TOK_ID TOK_COLON TOK_ID TOK_COLON TOK_ID
  ;
 
 syncDef
  : TOK_SYNC TOK_COLON syncConstraints attributeList
  | TOK_SYNC TOK_COLON syncConstraints
- ;
-
-
-attributeList
- : TOK_LBRACE attributes TOK_RBRACE
- | TOK_LBRACE TOK_RBRACE
- ;
-
-attributes
- : attribute
- | attribute TOK_COLON attributes
- ;
-
-attribute
- : TOK_TEXT TOK_COLON TOK_TEXT
- | TOK_TEXT TOK_COLON
  ;
 
 syncConstraints
@@ -118,6 +132,68 @@ syncConstraints1
  ;
 
 syncConstraint
- : TOK_PROCESS TOK_AT TOK_EVENT
- | TOK_PROCESS TOK_AT TOK_EVENT TOK_QMARK
+ : TOK_ID TOK_AT TOK_ID
+ | TOK_PROCESS TOK_AT TOK_ID TOK_QMARK
+ ;
+
+attributeList
+ : TOK_LBRACE attributes TOK_RBRACE
+ | TOK_LBRACE TOK_RBRACE
+ ;
+
+attributes
+ : attribute
+ | attribute TOK_COLON attributes
+ ;
+
+attribute
+ : TOK_INIT TOK_COLON
+ | TOK_LABELS TOK_COLON labelsList
+ | TOK_INVAR TOK_COLON constraints
+ | TOK_COMMIT TOK_COLON
+ | TOK_URGENT TOK_COLON
+ | TOK_PROV TOK_COLON constraints
+ | TOK_DO TOK_COLON reset
+ ;
+
+labelsList
+ : TOK_ID
+ | TOK_ID TOK_COMMA labelsList
+ ;
+
+constraints
+ : constraint
+ | constraint TOK_AND constraints
+ ;
+
+constraint
+ : TOK_ID cmp formula
+ | formula cmp TOK_ID cmp formula
+ ;
+
+formula
+ : TOK_INTEGER
+ | TOK_INTEGER maths formula
+ ;
+
+reset
+ : TOK_ID TOK_SET TOK_INTEGER
+ | TOK_ID TOK_SET TOK_INTEGER TOK_SEMICOLON reset
+ ;
+
+cmp
+ : TOK_EQ
+ | TOK_LT
+ | TOK_LEQ
+ | TOK_GEQ
+ | TOK_GT
+ | TOK_NEQ
+ ;
+
+maths
+ : TOK_PLUS
+ | TOK_MINUS
+ | TOK_MUL
+ | TOK_DIV
+ | TOK_PERCENT
  ;
--- src/parser/timedAutomata.js
+++ src/parser/timedAutomata.js
@@ -83,11 +83,30 @@ var timedAutomata = (function () {
     $V4 = [1, 19],
     $V5 = [1, 20],
     $V6 = [1, 21],
-    $V7 = [5, 20, 22, 23, 25, 26, 27, 28],
-    $V8 = [20, 22, 23, 25, 26, 27, 28],
-    $V9 = [1, 42],
-    $Va = [1, 55],
-    $Vb = [17, 32];
+    $V7 = [5, 20, 21, 22, 24, 25, 26, 27],
+    $V8 = [20, 21, 22, 24, 25, 26, 27],
+    $V9 = [1, 43],
+    $Va = [1, 57],
+    $Vb = [1, 58],
+    $Vc = [1, 59],
+    $Vd = [1, 60],
+    $Ve = [1, 61],
+    $Vf = [1, 62],
+    $Vg = [1, 63],
+    $Vh = [17, 36],
+    $Vi = [1, 87],
+    $Vj = [1, 90],
+    $Vk = [1, 92],
+    $Vl = [1, 95],
+    $Vm = [1, 101],
+    $Vn = [1, 102],
+    $Vo = [1, 103],
+    $Vp = [1, 104],
+    $Vq = [1, 105],
+    $Vr = [1, 106],
+    $Vs = [17, 36, 50, 56, 57, 58, 59, 60, 61],
+    $Vt = [18, 23],
+    $Vu = [17, 36, 50];
   var parser = {
     trace: function trace() {},
     yy: {},
@@ -108,27 +127,55 @@ var timedAutomata = (function () {
       syncDef: 15,
       TOK_SYSTEM: 16,
       TOK_COLON: 17,
-      TOK_TEXT: 18,
+      TOK_ID: 18,
       attributeList: 19,
       TOK_PROCESS: 20,
-      TOK_ID: 21,
-      TOK_EVENT: 22,
-      TOK_CLOCK: 23,
-      TOK_INTEGER: 24,
-      TOK_INT: 25,
-      TOK_LOCATION: 26,
-      TOK_EDGE: 27,
-      TOK_SYNC: 28,
-      syncConstraints: 29,
-      TOK_LBRACE: 30,
-      attributes: 31,
-      TOK_RBRACE: 32,
-      attribute: 33,
-      syncConstraint: 34,
-      syncConstraint1: 35,
-      syncConstraints1: 36,
-      TOK_AT: 37,
-      TOK_QMARK: 38,
+      TOK_EVENT: 21,
+      TOK_CLOCK: 22,
+      TOK_INTEGER: 23,
+      TOK_INT: 24,
+      TOK_LOCATION: 25,
+      TOK_EDGE: 26,
+      TOK_SYNC: 27,
+      syncConstraints: 28,
+      syncConstraint: 29,
+      syncConstraint1: 30,
+      syncConstraints1: 31,
+      TOK_AT: 32,
+      TOK_QMARK: 33,
+      TOK_LBRACE: 34,
+      attributes: 35,
+      TOK_RBRACE: 36,
+      attribute: 37,
+      TOK_INIT: 38,
+      TOK_LABELS: 39,
+      labelsList: 40,
+      TOK_INVAR: 41,
+      constraints: 42,
+      TOK_COMMIT: 43,
+      TOK_URGENT: 44,
+      TOK_PROV: 45,
+      TOK_DO: 46,
+      reset: 47,
+      TOK_COMMA: 48,
+      constraint: 49,
+      TOK_AND: 50,
+      cmp: 51,
+      formula: 52,
+      maths: 53,
+      TOK_SET: 54,
+      TOK_SEMICOLON: 55,
+      TOK_EQ: 56,
+      TOK_LT: 57,
+      TOK_LEQ: 58,
+      TOK_GEQ: 59,
+      TOK_GT: 60,
+      TOK_NEQ: 61,
+      TOK_PLUS: 62,
+      TOK_MINUS: 63,
+      TOK_MUL: 64,
+      TOK_DIV: 65,
+      TOK_PERCENT: 66,
       $accept: 0,
       $end: 1,
     },
@@ -137,21 +184,42 @@ var timedAutomata = (function () {
       5: 'TOK_EOF',
       16: 'TOK_SYSTEM',
       17: 'TOK_COLON',
-      18: 'TOK_TEXT',
+      18: 'TOK_ID',
       20: 'TOK_PROCESS',
-      21: 'TOK_ID',
-      22: 'TOK_EVENT',
-      23: 'TOK_CLOCK',
-      24: 'TOK_INTEGER',
-      25: 'TOK_INT',
-      26: 'TOK_LOCATION',
-      27: 'TOK_EDGE',
-      28: 'TOK_SYNC',
-      30: 'TOK_LBRACE',
-      32: 'TOK_RBRACE',
-      35: 'syncConstraint1',
-      37: 'TOK_AT',
-      38: 'TOK_QMARK',
+      21: 'TOK_EVENT',
+      22: 'TOK_CLOCK',
+      23: 'TOK_INTEGER',
+      24: 'TOK_INT',
+      25: 'TOK_LOCATION',
+      26: 'TOK_EDGE',
+      27: 'TOK_SYNC',
+      30: 'syncConstraint1',
+      32: 'TOK_AT',
+      33: 'TOK_QMARK',
+      34: 'TOK_LBRACE',
+      36: 'TOK_RBRACE',
+      38: 'TOK_INIT',
+      39: 'TOK_LABELS',
+      41: 'TOK_INVAR',
+      43: 'TOK_COMMIT',
+      44: 'TOK_URGENT',
+      45: 'TOK_PROV',
+      46: 'TOK_DO',
+      48: 'TOK_COMMA',
+      50: 'TOK_AND',
+      54: 'TOK_SET',
+      55: 'TOK_SEMICOLON',
+      56: 'TOK_EQ',
+      57: 'TOK_LT',
+      58: 'TOK_LEQ',
+      59: 'TOK_GEQ',
+      60: 'TOK_GT',
+      61: 'TOK_NEQ',
+      62: 'TOK_PLUS',
+      63: 'TOK_MINUS',
+      64: 'TOK_MUL',
+      65: 'TOK_DIV',
+      66: 'TOK_PERCENT',
     },
     productions_: [
       0,
@@ -175,23 +243,50 @@ var timedAutomata = (function () {
       [11, 6],
       [11, 5],
       [12, 12],
+      [12, 11],
       [13, 6],
       [13, 5],
       [14, 10],
       [14, 9],
       [15, 4],
       [15, 3],
-      [19, 3],
-      [19, 2],
+      [28, 3],
       [31, 1],
       [31, 3],
-      [33, 3],
-      [33, 2],
       [29, 3],
-      [36, 1],
-      [36, 3],
-      [34, 3],
-      [34, 4],
+      [29, 4],
+      [19, 3],
+      [19, 2],
+      [35, 1],
+      [35, 3],
+      [37, 2],
+      [37, 3],
+      [37, 3],
+      [37, 2],
+      [37, 2],
+      [37, 3],
+      [37, 3],
+      [40, 1],
+      [40, 3],
+      [42, 1],
+      [42, 3],
+      [49, 3],
+      [49, 5],
+      [52, 1],
+      [52, 3],
+      [47, 3],
+      [47, 5],
+      [51, 1],
+      [51, 1],
+      [51, 1],
+      [51, 1],
+      [51, 1],
+      [51, 1],
+      [53, 1],
+      [53, 1],
+      [53, 1],
+      [53, 1],
+      [53, 1],
     ],
     performAction: function anonymous(
       yytext,
@@ -223,12 +318,12 @@ var timedAutomata = (function () {
         14: 13,
         15: 14,
         20: $V0,
-        22: $V1,
-        23: $V2,
-        25: $V3,
-        26: $V4,
-        27: $V5,
-        28: $V6,
+        21: $V1,
+        22: $V2,
+        24: $V3,
+        25: $V4,
+        26: $V5,
+        27: $V6,
       },
       { 17: [1, 22] },
       { 1: [2, 1] },
@@ -245,12 +340,12 @@ var timedAutomata = (function () {
         14: 13,
         15: 14,
         20: $V0,
-        22: $V1,
-        23: $V2,
-        25: $V3,
-        26: $V4,
-        27: $V5,
-        28: $V6,
+        21: $V1,
+        22: $V2,
+        24: $V3,
+        25: $V4,
+        26: $V5,
+        27: $V6,
       },
       o($V7, [2, 5]),
       o($V7, [2, 6]),
@@ -268,66 +363,129 @@ var timedAutomata = (function () {
       { 17: [1, 30] },
       { 18: [1, 31] },
       { 5: [2, 4] },
-      { 21: [1, 32] },
-      { 21: [1, 33] },
-      { 24: [1, 34] },
-      { 24: [1, 35] },
-      { 20: [1, 36] },
-      { 20: [1, 37] },
-      { 20: [1, 40], 29: 38, 34: 39 },
-      o($V8, [2, 13], { 19: 41, 30: $V9 }),
-      o($V7, [2, 15], { 19: 43, 30: $V9 }),
-      o($V7, [2, 17], { 19: 44, 30: $V9 }),
-      { 17: [1, 45] },
+      { 18: [1, 32] },
+      { 18: [1, 33] },
+      { 23: [1, 34] },
+      { 23: [1, 35] },
+      { 18: [1, 36] },
+      { 18: [1, 37] },
+      { 18: [1, 40], 20: [1, 41], 28: 38, 29: 39 },
+      o($V8, [2, 13], { 19: 42, 34: $V9 }),
+      o($V7, [2, 15], { 19: 44, 34: $V9 }),
+      o($V7, [2, 17], { 19: 45, 34: $V9 }),
       { 17: [1, 46] },
       { 17: [1, 47] },
       { 17: [1, 48] },
-      o($V7, [2, 26], { 19: 49, 30: $V9 }),
-      { 17: [1, 50] },
-      { 37: [1, 51] },
+      { 17: [1, 49] },
+      o($V7, [2, 27], { 19: 50, 34: $V9 }),
+      { 17: [1, 51] },
+      { 32: [1, 52] },
+      { 32: [1, 53] },
       o($V8, [2, 12]),
-      { 18: $Va, 31: 52, 32: [1, 53], 33: 54 },
+      { 35: 54, 36: [1, 55], 37: 56, 38: $Va, 39: $Vb, 41: $Vc, 43: $Vd, 44: $Ve, 45: $Vf, 46: $Vg },
       o($V7, [2, 14]),
       o($V7, [2, 16]),
-      { 21: [1, 56] },
-      { 24: [1, 57] },
-      { 21: [1, 58] },
-      { 26: [1, 59] },
-      o($V7, [2, 25]),
-      { 35: [1, 60] },
-      { 22: [1, 61] },
-      { 32: [1, 62] },
-      o($V7, [2, 28]),
-      { 17: [1, 63], 32: [2, 29] },
-      { 17: [1, 64] },
-      o($V7, [2, 19], { 19: 65, 30: $V9 }),
-      { 17: [1, 66] },
-      o($V7, [2, 22], { 19: 67, 30: $V9 }),
-      { 17: [1, 68] },
-      o([5, 20, 22, 23, 25, 26, 27, 28, 30], [2, 33]),
-      { 17: [2, 36], 38: [1, 69] },
-      o($V7, [2, 27]),
-      { 18: $Va, 31: 70, 33: 54 },
-      o($Vb, [2, 32], { 18: [1, 71] }),
-      o($V7, [2, 18]),
-      { 24: [1, 72] },
-      o($V7, [2, 21]),
-      { 26: [1, 73] },
-      { 17: [2, 37] },
-      { 32: [2, 30] },
-      o($Vb, [2, 31]),
+      { 18: [1, 64] },
+      { 23: [1, 65] },
+      { 18: [1, 66] },
+      { 18: [1, 67] },
+      o($V7, [2, 26]),
+      { 30: [1, 68] },
+      { 18: [1, 69] },
+      { 18: [1, 70] },
+      { 36: [1, 71] },
+      o($V7, [2, 34]),
+      { 17: [1, 72], 36: [2, 35] },
+      { 17: [1, 73] },
       { 17: [1, 74] },
       { 17: [1, 75] },
-      { 24: [1, 76] },
-      { 22: [1, 77] },
+      { 17: [1, 76] },
+      { 17: [1, 77] },
       { 17: [1, 78] },
-      o($V7, [2, 24], { 19: 79, 30: $V9 }),
-      { 21: [1, 80] },
-      o($V7, [2, 23]),
-      { 19: 81, 30: $V9 },
+      { 17: [1, 79] },
+      o($V7, [2, 19], { 19: 80, 34: $V9 }),
+      { 17: [1, 81] },
+      o($V7, [2, 23], { 19: 82, 34: $V9 }),
+      { 17: [1, 83] },
+      o([5, 20, 21, 22, 24, 25, 26, 27, 34], [2, 28]),
+      { 17: [2, 31] },
+      { 33: [1, 84] },
+      o($V7, [2, 33]),
+      { 35: 85, 37: 56, 38: $Va, 39: $Vb, 41: $Vc, 43: $Vd, 44: $Ve, 45: $Vf, 46: $Vg },
+      o($Vh, [2, 37]),
+      { 18: $Vi, 40: 86 },
+      { 18: $Vj, 23: $Vk, 42: 88, 49: 89, 52: 91 },
+      o($Vh, [2, 40]),
+      o($Vh, [2, 41]),
+      { 18: $Vj, 23: $Vk, 42: 93, 49: 89, 52: 91 },
+      { 18: $Vl, 47: 94 },
+      o($V7, [2, 18]),
+      { 23: [1, 96] },
+      o($V7, [2, 22]),
+      { 18: [1, 97] },
+      { 17: [2, 32] },
+      { 36: [2, 36] },
+      o($Vh, [2, 38]),
+      o($Vh, [2, 44], { 48: [1, 98] }),
+      o($Vh, [2, 39]),
+      o($Vh, [2, 46], { 50: [1, 99] }),
+      { 51: 100, 56: $Vm, 57: $Vn, 58: $Vo, 59: $Vp, 60: $Vq, 61: $Vr },
+      { 51: 107, 56: $Vm, 57: $Vn, 58: $Vo, 59: $Vp, 60: $Vq, 61: $Vr },
+      o($Vs, [2, 50], { 53: 108, 62: [1, 109], 63: [1, 110], 64: [1, 111], 65: [1, 112], 66: [1, 113] }),
+      o($Vh, [2, 42]),
+      o($Vh, [2, 43]),
+      { 54: [1, 114] },
+      { 17: [1, 115] },
+      { 17: [1, 116] },
+      { 18: $Vi, 40: 117 },
+      { 18: $Vj, 23: $Vk, 42: 118, 49: 89, 52: 91 },
+      { 23: $Vk, 52: 119 },
+      o($Vt, [2, 54]),
+      o($Vt, [2, 55]),
+      o($Vt, [2, 56]),
+      o($Vt, [2, 57]),
+      o($Vt, [2, 58]),
+      o($Vt, [2, 59]),
+      { 18: [1, 120] },
+      { 23: $Vk, 52: 121 },
+      { 23: [2, 60] },
+      { 23: [2, 61] },
+      { 23: [2, 62] },
+      { 23: [2, 63] },
+      { 23: [2, 64] },
+      { 23: [1, 122] },
+      { 23: [1, 123] },
+      { 18: [1, 124] },
+      o($Vh, [2, 45]),
+      o($Vh, [2, 47]),
+      o($Vu, [2, 48]),
+      { 51: 125, 56: $Vm, 57: $Vn, 58: $Vo, 59: $Vp, 60: $Vq, 61: $Vr },
+      o($Vs, [2, 51]),
+      o($Vh, [2, 52], { 55: [1, 126] }),
+      { 17: [1, 127] },
+      o($V7, [2, 25], { 19: 128, 34: $V9 }),
+      { 23: $Vk, 52: 129 },
+      { 18: $Vl, 47: 130 },
+      { 18: [1, 131] },
+      o($V7, [2, 24]),
+      o($Vu, [2, 49]),
+      o($Vh, [2, 53]),
+      o($V7, [2, 21], { 19: 132, 34: $V9 }),
       o($V7, [2, 20]),
     ],
-    defaultActions: { 5: [2, 1], 6: [2, 2], 23: [2, 4], 69: [2, 37], 70: [2, 30] },
+    defaultActions: {
+      5: [2, 1],
+      6: [2, 2],
+      23: [2, 4],
+      69: [2, 31],
+      84: [2, 32],
+      85: [2, 36],
+      109: [2, 60],
+      110: [2, 61],
+      111: [2, 62],
+      112: [2, 63],
+      113: [2, 64],
+    },
     parseError: function parseError(str, hash) {
       if (hash.recoverable) {
         this.trace(str);
@@ -845,54 +1003,138 @@ var timedAutomata = (function () {
             return 'TOK_EOL';
             break;
           case 3:
-            return 23;
-            break;
-          case 4:
-            return 27;
-            break;
-          case 5:
             return 22;
             break;
-          case 6:
+          case 4:
+            return 26;
+            break;
+          case 5:
             return 21;
+            break;
+          case 6:
+            return 24;
             break;
           case 7:
             return 25;
             break;
           case 8:
-            return 26;
-            break;
-          case 9:
             return 20;
             break;
-          case 10:
-            return 28;
+          case 9:
+            return 27;
             break;
-          case 11:
+          case 10:
             return 16;
             break;
-          case 12:
-            return 18;
-            break;
-          case 13:
-            return 24;
-            break;
-          case 14:
-            return 17;
-            break;
-          case 15:
-            return 37;
-            break;
-          case 16:
+          case 11:
             return 38;
             break;
+          case 12:
+            return 39;
+            break;
+          case 13:
+            return 41;
+            break;
+          case 14:
+            return 43;
+            break;
+          case 15:
+            return 44;
+            break;
+          case 16:
+            return 45;
+            break;
           case 17:
-            return 30;
+            return 46;
             break;
           case 18:
-            return 32;
+            return 'TOK_IF';
             break;
           case 19:
+            return 'TOK_WHILE';
+            break;
+          case 20:
+            return 'TOK_NOP';
+            break;
+          case 21:
+            return 'TOK_LOCAL';
+            break;
+          case 22:
+            return 23;
+            break;
+          case 23:
+            return 18;
+            break;
+          case 24:
+            return 17;
+            break;
+          case 25:
+            return 32;
+            break;
+          case 26:
+            return 'TOK_EXMARK';
+            break;
+          case 27:
+            return 33;
+            break;
+          case 28:
+            return 34;
+            break;
+          case 29:
+            return 36;
+            break;
+          case 30:
+            return 48;
+            break;
+          case 31:
+            return 50;
+            break;
+          case 32:
+            return 62;
+            break;
+          case 33:
+            return 63;
+            break;
+          case 34:
+            return 64;
+            break;
+          case 35:
+            return 65;
+            break;
+          case 36:
+            return 66;
+            break;
+          case 37:
+            return 56;
+            break;
+          case 38:
+            return 61;
+            break;
+          case 39:
+            return 58;
+            break;
+          case 40:
+            return 59;
+            break;
+          case 41:
+            return 57;
+            break;
+          case 42:
+            return 60;
+            break;
+          case 43:
+            return 54;
+            break;
+          case 44:
+            return 'TOK_LBRACKET';
+            break;
+          case 45:
+            return 'TOK_RBRACKET';
+            break;
+          case 46:
+            return 55;
+            break;
+          case 47:
             return 5;
             break;
         }
@@ -904,23 +1146,57 @@ var timedAutomata = (function () {
         /^(?:clock\b)/,
         /^(?:edge\b)/,
         /^(?:event\b)/,
-        /^(?:identifier\b)/,
         /^(?:int\b)/,
         /^(?:location\b)/,
         /^(?:process\b)/,
         /^(?:sync\b)/,
         /^(?:system\b)/,
-        /^(?:[a-zA-Z]([a-zA-Z0-9_])*)/,
+        /^(?:initial\b)/,
+        /^(?:labels\b)/,
+        /^(?:invariant\b)/,
+        /^(?:committed\b)/,
+        /^(?:urgent\b)/,
+        /^(?:provided\b)/,
+        /^(?:do\b)/,
+        /^(?:if\b)/,
+        /^(?:while\b)/,
+        /^(?:nop\b)/,
+        /^(?:local\b)/,
         /^(?:[0-9]+)/,
+        /^(?:[a-zA-Z]([a-zA-Z0-9_])*)/,
         /^(?::)/,
         /^(?:@)/,
+        /^(?:!)/,
         /^(?:\?)/,
         /^(?:\{)/,
         /^(?:\})/,
+        /^(?:,)/,
+        /^(?:&&)/,
+        /^(?:\+)/,
+        /^(?:-)/,
+        /^(?:\*)/,
+        /^(?:\/)/,
+        /^(?:%)/,
+        /^(?:==)/,
+        /^(?:!=)/,
+        /^(?:<=)/,
+        /^(?:>=)/,
+        /^(?:<)/,
+        /^(?:>)/,
+        /^(?:=)/,
+        /^(?:\()/,
+        /^(?:\))/,
+        /^(?:;)/,
         /^(?:$)/,
       ],
       conditions: {
-        INITIAL: { rules: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19], inclusive: true },
+        INITIAL: {
+          rules: [
+            0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28,
+            29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47,
+          ],
+          inclusive: true,
+        },
       },
     };
     return lexer;
@@ -952,3 +1228,5 @@ if (typeof require !== 'undefined' && typeof exports !== 'undefined') {
     exports.main(process.argv.slice(1));
   }
 }
+
+export default timedAutomata;
