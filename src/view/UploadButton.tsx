ffb1349b6f84a066106a63aaba9172b058213d01
Josef552 <105931716+Josef552@users.noreply.github.com>
2024-07-14 20:06:19 +0200

    UPDATE

--- src/view/UploadButton.tsx
+++ src/view/UploadButton.tsx
@@ -1,20 +1,31 @@
 import React from 'react';
 
 import timedAutomata from '../parser/timedAutomata';
+import { AnalysisViewModel } from '../viewmodel/AnalysisViewModel.ts';
 console.log(timedAutomata);
 
 export interface OpenedDocs {
-  //unbenennen in OpenedDocs oder so
-  //fileContents: string;
+  viewModel: AnalysisViewModel; //für update Locations iwie?
+  // brauche dann noch so ne "mapParsedDataToTA" Funktion und kann dann aus mappingUtils die Fkt aufrufen
+  //fileContents: string; //<-- brauch ich sowas???
 }
+
+const parseFile = async (fileContent: string) => {
+  const parser = new timedAutomata.Parser();
+
+  const parsedData = parser.parse(fileContent);
+  //console.log(parsedData);
+
+  return parsedData;
+};
 
 const UploadButton: React.FC<OpenedDocs> = () => {
   const handleClick = (uploadedFileEvent: React.ChangeEvent<HTMLInputElement>) => {
     const inputElem = uploadedFileEvent.target as HTMLInputElement & {
       files: FileList;
     };
-    //TODO der überprüft ja momentan noch nicht wirklich, ob das eine .tck Datei ist...
 
+    //TODO der überprüft ja momentan noch nicht wirklich, ob das eine .tck Datei ist...
     if (!inputElem.files[0] || !inputElem.files[0].name.endsWith('.tck')) {
       console.log('Invalid or no File');
       return;
@@ -22,41 +33,21 @@ const UploadButton: React.FC<OpenedDocs> = () => {
     console.log(inputElem.files[0]); //eingelesene File
 
     const fileReader = new FileReader();
-    fileReader.onload = () => {
+    fileReader.onload = async () => {
       const fileContent = fileReader.result as string;
       console.log(fileContent);
 
-      //TODO das einkommentieren, sobald import geht
-      const parser = new timedAutomata.Parser();
-
-      const parsedData = parser.parse(fileContent);
-      console.log(parsedData);
+      try {
+        const parsedData = await parseFile(fileContent);
+        console.log(parsedData); //contents of parsed File
+      } catch (error) {
+        console.error(error);
+      }
     };
     fileReader.readAsText(inputElem.files[0]);
 
     //TODO Muss ich die File noch irgendwo anders abspeichern?
   };
-
-  /**
-  const myPromise: Promise<any> = new Promise((resolve, reject)) => {
-    parser.parse(inputFile);
-  }
-
-  //oder "File", wenn ich den fileReader hier mit reinpacke
-  async function parseData(inputFile: string){
-    try{
-      const parsedData = await parser.parse(inputFile);
-      console.log(parsedData);
-      //ODER:
-      // return parser.parse(inputFile);
-      console.log('a');
-    }
-    catch(error){
-      console.log(error);
-    }
-
-  }
-   **/
 
   //TODO hier noch das "Upload file" in diese Localization file tun
   return (
